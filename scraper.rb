cefdb97d9ef913a81557a73163b0ef1da4847dbd
Tony Bowden <tony@tmtm.com>
2016-10-01 15:54:10 +0100

    Include members who have been replaced

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,14 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+replaced = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
+  xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[4][contains(.,"Menggantikan")]//a[not(@class="new")]/@title',
+)
+
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
   xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[1]//a[not(@class="new")]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { id: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(names: { id: names | replaced }, output: false)
