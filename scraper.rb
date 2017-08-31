b01464fab2b3fed532863043a174d84592c360fe
Tony Bowden <tony@tmtm.com>
2017-08-31 07:41:31 +0100

    Also fetch by Category

--- scraper.rb
+++ scraper.rb
@@ -3,14 +3,17 @@
 
 require 'wikidata/fetcher'
 
-replaced = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
-  xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[4][contains(.,"Menggantikan")]//a[not(@class="new")]/@title',
-)
-
 names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
   xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[1]//a[not(@class="new")]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { id: names | replaced }, output: false)
+replaced = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
+  xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[4][contains(.,"Menggantikan")]//a[not(@class="new")]/@title',
+)
+
+category_2014 = WikiData::Category.new('Kategori:Anggota DPR 2014-2019', 'id').member_titles
+category_2009 = WikiData::Category.new('Kategori:Anggota DPR 2009-2014', 'id').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { id: names | replaced | category_2014 | category_2009 })
