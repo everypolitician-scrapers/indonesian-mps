c8a4e3daa77889f178133d840b3d9b7aeb880b79
Tony Bowden <tony@tmtm.com>
2017-09-01 08:19:12 +0100

    Also scrape anyone already matched

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
+
+existing = EveryPolitician::Index.new.country("Indonesia").lower_house.popolo.persons.map(&:wikidata).compact
 
 names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
@@ -16,4 +19,4 @@ replaced = EveryPolitician::Wikidata.wikipedia_xpath(
 category_2014 = WikiData::Category.new('Kategori:Anggota DPR 2014-2019', 'id').member_titles
 category_2009 = WikiData::Category.new('Kategori:Anggota DPR 2009-2014', 'id').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { id: names | replaced | category_2014 | category_2009 })
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { id: names | replaced | category_2014 | category_2009 })
