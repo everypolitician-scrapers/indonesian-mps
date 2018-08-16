efeb4213453b77643bad3e6ebcfeb120e3104811
Tony Bowden <tony@tmtm.com>
2018-08-16 16:34:31 +0100

    Update for changed URL and layout

--- scraper.rb
+++ scraper.rb
@@ -7,13 +7,13 @@ require 'wikidata/fetcher'
 existing = EveryPolitician::Index.new.country("Indonesia").lower_house.popolo.persons.map(&:wikidata).compact
 
 names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
-  xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[1]//a[not(@class="new")]/@title',
+  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_Dewan_Perwakilan_Rakyat_Republik_Indonesia_2014%E2%80%932019',
+  xpath: '//table[.//tr[th[contains(.,"Nama")]]]//tr[td]//td[1]//a[not(@class="new")]/@title',
 )
 
 replaced = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_DPR_RI_2014–2019',
-  xpath: '//table[.//tr[th[.="Nama"]]]//tr[td]//td[4][contains(.,"Menggantikan")]//a[not(@class="new")]/@title',
+  url: 'https://id.wikipedia.org/wiki/Daftar_anggota_Dewan_Perwakilan_Rakyat_Republik_Indonesia_2014%E2%80%932019',
+  xpath: '//table[.//tr[th[contains(.,"Nama")]]]//tr[td]//td[4][contains(.,"Menggantikan")]//a[not(@class="new")]/@title',
 )
 
 category_2014 = WikiData::Category.new('Kategori:Anggota DPR 2014-2019', 'id').member_titles
